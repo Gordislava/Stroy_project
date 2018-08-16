7bdd83330ac086aa583cbe0855cbd9f164c939e4
gordislava <gordislava@gmail.com>
2018-08-16 09:44:59 +0300

    Главная, статьи/статья, услуги/услуга, о нас

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// $(document).ready(function() {
+//
+// 	$(".fa-bars").click(function() {
+// 		$(".main-menu__list").slideToggle();
+// 	});
+//
+// });
+//
+// $(document).ready(function() {
+//
+// 	$(".header-left").click(function() {
+// 		$(".footer-menu--left").slideToggle();
+// 	});
+//
+// });
+//
+// $(document).ready(function() {
+//
+// 	$(".header-right").click(function() {
+// 		$(".footer-menu-right").slideToggle();
+// 		if(window.innerWidth>600){
+// 			$('.footer-menu-right').css('display','flex');
+// 		}else{
+// 			$('.footer-menu-right').css('display','none');
+// 		}
+// 	});
+//
+// });
